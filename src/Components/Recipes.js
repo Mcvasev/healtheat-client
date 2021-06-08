e2d84fd0d86298ce1942257b8351d6fdfebf82f7
Islam Alaev <alaev095@bk.ru>
2021-06-08 14:44:03 +0300

    MarkIlya

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -4,26 +4,38 @@ export default function Recipes({ imgrec, title, description, ingredients, break
     return (
         <div className="containerRec">
             <div className="bodyRecipes">
-                <img className="imgRecipeBody" src={imgrec} />
                 <div className="nameRecipe">
                     <div className="titleButton">
-                        <h4>{title}</h4>
-                        <button className="buttonRandomRecipe" onClick={breakRandom}> сменить {type} </button>
+                        <img className="imgRecipeBody" src={imgrec} />
+                        <div>
+                            <p>{type}</p>
+                            <h5 className="recipeName">{title}</h5>
+                            {/* <p>{ingredients.map((item) => (
+                                 item.title
+                                ))},</p> */}
+                        </div>
+
                     </div>
+                    <button className="buttonRandomRecipe" onClick={breakRandom}> сменить {type} </button>
                     <button className="buttonWindow" onClick={flag}>показать рецепт</button>
                     {flagWindow ?
                         <div>
-                            <p className="recipeDescription">{description}</p>
+                            <h3>Ингредиенты:</h3>
                             <ul>
                                 {ingredients.map((item) => (
                                     <li key={item.title}>{item.title} {item.amount} {item.units} </li>
                                 ))}
                             </ul>
+                            <h3>Рецепт:</h3>
+                            <p className="recipeDescription">{description}</p>
+
                         </div>
                         : <p></p>
                     }
                 </div>
             </div>
+
+
             <hr />
         </div>
     )
